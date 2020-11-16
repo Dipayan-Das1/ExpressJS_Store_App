2fe724e89565b49bf0b43fa3bbfafaeab4e0ac05
Dipayan-Das1 <dipayan1.das@gmai.com>
2020-11-16 09:41:08 +0530

    Update to mongo

--- util/database.js
+++ util/database.js
@@ -1,6 +1,55 @@
-//import mysql2
-const Sequelize = require('sequelize');
+const mongodb = require("mongodb");
+const mongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('storedb','STORE_USR','STORE_USR_PWD',{host:'localhost',dialect:'mysql'});
+let connection; 
 
-module.exports = sequelize;
+let url = "mongodb://localhost:27017/storedb";
+let usr;
+
+const mongoConnect = () => { return mongoClient.connect(url).then(client => {
+    connection = client.db("storedb");
+    return connection;
+}).then(
+    conn => {
+        conn.collection("users").findOne({name:"admin"}).then(user => {
+         if(user)
+         {
+             usr = user._id;
+             return user;
+
+         }   
+         else{
+             console.log("Create admin user");
+            return conn.collection("users").insertOne({name:"admin",email:"admin@admin.com"}).then(res =>{
+                usr = res.insertedId;
+            });
+         }
+        }).then(res => {
+            
+            console.log("Set user to "+usr)
+            return usr;
+        }).then(res => {
+            connection.createCollection("products").then(res => {
+                console.log("products db created");
+            }).catch(err =>{
+                
+            });
+            return res;
+        }).then(res => {
+                    connection.createCollection("orders").then(res => {
+                    console.log("orders db created");
+                }).catch(err =>{
+                    
+                });
+                return res;
+            })
+            
+    }
+)};
+
+const getUser = () => usr;
+const getConnection = () => connection;
+
+module.exports.mongoConnect = mongoConnect;
+module.exports.getConnection = getConnection; 
+module.exports.getUser = getUser;
