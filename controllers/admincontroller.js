e3c6a0eeab86f291fbee8f67346db9852aa8ca22
Dipayan-Das1 <dipayan1.das@gmai.com>
2020-11-19 10:15:14 +0530

    update admin contoller

--- controllers/admincontroller.js
+++ controllers/admincontroller.js
@@ -1,12 +1,14 @@
 const Product = require('../models/productmodel');
 
 module.exports.getAddProduct = (req,res,next) => {
-    res.render("admin/edit-product",{pageTitle:'Add Product',path:'/add-product',editing:false});
+    let authenticated = req.session.isLoggedin ? true : false;
+    res.render("admin/edit-product",{pageTitle:'Add Product',path:'/add-product',editing:false,isAuthenticated:authenticated});
 }
 
 module.exports.addProduct = (req,res,next) => {
+    let authenticated = req.session.isLoggedin ? true : false;
     console.log("Add product ");
-    let product = new Product(req.body.title,req.body.price,req.body.description,req.user);
+    let product = new Product({title:req.body.title,price:req.body.price,description:req.body.description,createdBy:req.user});
     product.save().then(prod => {
         console.log("Product save");
         res.redirect('/manage/products');
@@ -18,10 +20,15 @@ module.exports.addProduct = (req,res,next) => {
 }
 
 module.exports.editProduct = (req,res,next) => {
+    let authenticated = req.session.isLoggedin ? true : false;
     //body-parser parses the request and converts it to json format access body by req.body
     console.log("Edit product");
-    let product = new Product(req.body.title,req.body.price,req.body.description,req.user,req.body.id);
-    product.update().then(prod => {
+    Product.findById(req.body.id).then((prod) => {
+        prod.title = req.body.title;
+        prod.description = req.body.description;
+        prod.price = req.body.price;
+        return prod.save();
+    }).then(prod => {
         res.redirect('/manage/products');
     }).catch(err => {
         console.log(err);
@@ -30,11 +37,12 @@ module.exports.editProduct = (req,res,next) => {
 }
 
 module.exports.getEditProduct = (req,res,next) => {
+    let authenticated = req.session.isLoggedin ? true : false;
     console.log("Inside edit product "+req.params.productId);
-    const product = Product.getById(req.params.productId).then((prod) => {
+    const product = Product.findById(req.params.productId).then((prod) => {
         if(prod)
         {
-            res.render("admin/edit-product",{pageTitle:'Edit Product',path:'/edit-product',product:prod,editing:true}); 
+            res.render("admin/edit-product",{pageTitle:'Edit Product',path:'/edit-product',product:prod,editing:true,isAuthenticated:authenticated}); 
         }
         else{
             res.redirect('/manage/products');
@@ -47,9 +55,9 @@ module.exports.getEditProduct = (req,res,next) => {
 
 
 module.exports.deleteProduct = (req,res,next) => {
-    
+    let authenticated = req.session.isLoggedin ? true : false;
     console.log("Inside delete product "+req.body.productId);
-    Product.delete(req.body.productId).then(resp => {
+    Product.findByIdAndDelete(req.body.productId).then(resp => {
         res.redirect('/manage/products');
       }).catch(err => {
         console.log(err);
@@ -58,8 +66,11 @@ module.exports.deleteProduct = (req,res,next) => {
 }
 
 module.exports.getProducts = (req,res,next) => {
-    Product.findAllByUser(req.user).then(rows => {
-        res.render("admin/products",{pageTitle:'Admin Products page',path:'/admin-product',products:rows});
+    let authenticated = req.session.isLoggedin ? true : false;
+    console.log("Inside get products")
+    Product.find({createdBy: req.user}).then(rows => {
+        console.log(rows);
+        res.render("admin/products",{pageTitle:'Admin Products page',path:'/admin-product',products:rows,isAuthenticated:authenticated});
     }).catch(err => {
         console.log(err);
     });  
